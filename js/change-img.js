daf8c008edf01c49641f9545d11376ec34615b49
Andrea-col97 <80856248+Andrea-col97@users.noreply.github.com>
2021-04-21 10:12:50 -0500

    Add files via upload

---
+++ js/change-img.js
@@ -0,0 +1,116 @@
+$(document).ready(function() {
+    
+    /******** Evolution ***********/
+    $( "img#lancerEvo" ).mouseenter(function() {
+        $(this).attr('src', 'img/pac3.PNG');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/pac4.PNG');
+    });
+    
+    /***************Pac-Man y fantasmas*************/
+    $( "img#L200" ).mouseenter(function() {
+        $(this).attr('src', 'img/amix1.PNG');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/amix3.PNG');
+    });
+    
+    
+    /***************LOS INICIOS DE MARIO BROS*************/
+    $( "img#Navara" ).mouseenter(function() {
+        $(this).attr('src', 'img/mario2.PNG');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/mario3.PNG');
+    });
+    
+    /*************** >SUPER MARIO WORLD *************/
+    $( "img#Frontier" ).mouseenter(function() {
+        $(this).attr('src', 'img/mariow1.PNG');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/mariow4.PNG');
+    });
+    
+    /*************** Hilux *************/
+    $( "img#Hilux" ).mouseenter(function() {
+        $(this).attr('src', 'img/Hilux2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Hilux1.jpg');
+    });
+    
+    /*************** Celica *************/
+    $( "img#Celica" ).mouseenter(function() {
+        $(this).attr('src', 'img/Celica2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Celica1.jpg');
+    });
+    
+    /*************** Supra *************/
+    $( "img#Supra" ).mouseenter(function() {
+        $(this).attr('src', 'img/Supra2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Supra1.jpg');
+    });
+    
+    /*************** Civic *************/
+    $( "img#Civic" ).mouseenter(function() {
+        $(this).attr('src', 'img/Civic2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Civic1.jpg');
+    });
+    
+    /*************** CR-V *************/
+    $( "img#Crv" ).mouseenter(function() {
+        $(this).attr('src', 'img/Cr-v2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Cr-v1.jpg');
+    });
+    
+    /*************** Mazda 3 *************/
+    $( "img#Mazda3" ).mouseenter(function() {
+        $(this).attr('src', 'img/Mazda32.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Mazda31.jpg');
+    });
+    
+    /*************** Mazda RX-7 *************/
+    $( "img#Rx-7" ).mouseenter(function() {
+        $(this).attr('src', 'img/Rx-72.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Rx-71.jpg');
+    });
+    
+    /*************** Mazda RX-8 *************/
+    $( "img#Rx-8" ).mouseenter(function() {
+        $(this).attr('src', 'img/Rx-82.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Rx-81.jpg');
+    });
+    
+    /*************** Impreza *************/
+    $( "img#Impreza" ).mouseenter(function() {
+        $(this).attr('src', 'img/Impreza2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Impreza1.jpg');
+    });
+    
+    /*************** XV *************/
+    $( "img#Xv" ).mouseenter(function() {
+        $(this).attr('src', 'img/Xv2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Xv1.jpg');
+    });
+    
+});
